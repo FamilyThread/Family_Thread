a2fd3dfd36c1a4c7f25e3242525f6610964c739d
s-Aura-v <slamich2@oswego.edu>
2024-03-11 18:49:44 -0400

    Home Skeleton Complete
    Created 2 components:
    Featured/Recent Tree
    
    Updated:
    Home Page

---
+++ src/components/FeaturedTrees.tsx
@@ -0,0 +1,18 @@
+import '../styles/displayTrees.css'
+function FeaturedTrees() {
+    return (
+        <>
+            <div className="trees">
+                <h2 className="tree-text">Featured Trees</h2>
+                <div className="display-trees">
+                    <div className="tree"> tree 1 </div>
+                    <div className="tree"> tree 2 </div>
+                    <div className="tree"> tree 3 </div>
+                    <div className="tree"> tree 4 </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FeaturedTrees;
