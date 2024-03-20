4567161d7be63f6c10c10d4876c2001f5d7ca9ed
cuceta <123569880+cuceta@users.noreply.github.com>
2024-03-20 01:28:44 -0400

    View trees page basics

--- src/pages/ViewTrees.tsx
+++ src/pages/ViewTrees.tsx
@@ -1,10 +1,22 @@
-import {NavigationBar} from "../components/NavigationBar.tsx";
+import {NavigationBar} from "../../../../Desktop/CSC380/Family_Thread-main/src/components/NavigationBar.tsx";
+import {FeaturedTrees} from "../../../../Desktop/CSC380/Family_Thread-main/src/components/FeaturedTrees.tsx";
+import {RecentTree} from "../../../../Desktop/CSC380/Family_Thread-main/src/components/RecentTree.tsx";
+import {MyTrees} from "../../../../Desktop/CSC380/Family_Thread-main/src/components/MyTrees.tsx";
 
 export function ViewTrees() {
     return (
         <>
+            <style>
+                {document.body.style.backgroundColor = '#6EA07F'};
+            </style>
+
             <NavigationBar />
-            <h1>ViewTrees</h1>
+
+            <div className="">
+                <MyTrees />
+                <RecentTree />
+                <FeaturedTrees />
+            </div>
         </>
     );
 }
