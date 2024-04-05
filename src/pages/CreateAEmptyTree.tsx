a5570408fdaaacae28916d635df7e1bd454c2ae1
LuisMed674 <lmedina@oswego.edu>
2024-04-04 22:16:32 -0400

    added Buttons
    Buttons for Upload and Template 1

---
+++ src/pages/CreateAEmptyTree.tsx
@@ -0,0 +1,28 @@
+import {NavigationBar} from "../components/NavigationBar.tsx";
+import FamilyTreeChart from "../components/FamilyTreeChart.ts";
+
+export function CreateAEmptyTree() {
+    return (
+        <>
+            <NavigationBar/>
+
+            <div style={{height: "100%"}}>
+
+                <FamilyTreeChart nodes={[
+                    { id: 1, pids: [2], name: 'Lia', gender: 'female', img: 'https://i.ibb.co/NCrWCBr/IMG-5982.jpg'},
+                    { id: 2, pids: [1], name: 'Soney', gender: 'male', img: 'https://i.ibb.co/h8s31DY/Screenshot-2024-03-28-at-11-53-13-PM.png' },
+                    { id: 3, mid: 1, pids: [4], fid: 2, name: 'Saurav', gender: 'male', img: 'https://i.ibb.co/j3Y9cDW/Screenshot-2024-03-29-at-12-05-46-AM.png' },
+                    { id: 4, pids: [3], name: 'Jessica', gender: 'male', img: 'https://www.shape.com/thmb/ogUySuD3fA6mdNl21sq5wMi7g-8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Jessica-Alba-Interview-GettyImages-1242827004-c6ed09b1faa04c9db646700e3afa484e.jpg' },
+                    { id: 5, mid: 1, fid: 2,pids:[6,7,8,9,10], name: 'Kerrie L', gender: 'female', img: 'https://i.ibb.co/72yHRht/Screenshot-2024-03-29-at-12-02-30-AM.png'  },
+                    { id: 6, pids: [5], name: 'Henry1', gender: 'male', img: 'https://cdn.britannica.com/34/254634-050-C62ACCB9/British-Actor-Henry-Cavill-February-2024.jpg'  },
+                    { id: 7, pids: [5], name: 'Henry2', gender: 'male', img: 'https://cdn.britannica.com/34/254634-050-C62ACCB9/British-Actor-Henry-Cavill-February-2024.jpg'  },
+                    { id: 8, pids: [5], name: 'Henry3', gender: 'male', img: 'https://cdn.britannica.com/34/254634-050-C62ACCB9/British-Actor-Henry-Cavill-February-2024.jpg'  },
+                    { id: 9, pids: [5], name: 'Henry4', gender: 'male', img: 'https://cdn.britannica.com/34/254634-050-C62ACCB9/British-Actor-Henry-Cavill-February-2024.jpg'  },
+                    { id: 10, pids: [5], name: 'Henry5', gender: 'male', img: 'https://cdn.britannica.com/34/254634-050-C62ACCB9/British-Actor-Henry-Cavill-February-2024.jpg'  },
+                    // { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
+                ]} />
+            </div>
+
+        </>
+    );
+}
--- src/pages/CreateTrees.tsx
+++ src/pages/CreateTrees.tsx
@@ -4,10 +4,10 @@ import {backend_url} from "../assets/constant.ts";
 import {useState} from "react";
 import {Separator} from "../components/separator.tsx";
 import importLogo from "../assets/import.png";
+import ThumbsUp from "../assets/ThumbsUp.png";
 import "../styles/createTrees.css";
 
 export function CreateTrees() {
-
     const [treeName, setTreeName] = useState("");
 
     const createNewTree = async () => {
@@ -18,6 +18,12 @@ export function CreateTrees() {
         console.log(data);
         return data
     }
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        // Process the file upload here
+        console.log(file);
+    };
 
     return (
         <>
@@ -40,46 +46,45 @@ export function CreateTrees() {
                         />
                     </label>
                     <input type="submit" value="Submit"/>
-
                 </form>
 
                 <Separator/>
 
                 <div className="external">
-
-                    <div className="rect-container">
+                    <a className="rect-container" href="../createAEmptyTree">
                         <div className="white-box">
-                            {/*<img src={importLogo} alt="Import A Tree"/>*/}
+                            <img src={ThumbsUp} alt="Import A Tree"/>
                         </div>
-                        <div className="box-caption">
+                        <div className="box-caption" >
                             Create an empty tree
                         </div>
-                    </div>
+                    </a>
 
-                    <div className="rect-container">
+                    {/* Upload button replacing the second rect-container */}
+                    <label className="rect-container">
                         <div className="white-box">
-                            <img src={importLogo} alt="Import A Tree"/>
+                            <input type="file" onChange={handleFileUpload} style={{ display: 'none' }} />
+                            <img src={importLogo} alt="Import A Tree" style={{ cursor: 'pointer' }} />
                         </div>
                         <div className="box-caption">
                             Import Data
                         </div>
-                    </div>
+                    </label>
                 </div>
                 <Separator/>
 
-                <h2 className="title"> Templates </h2>
-
+                <h2 className="title">Templates</h2>
                 <div className="template-grid">
-                    <div className="grid-col">
+                    <a className="grid-col" href="../displayTrees">
                         <div className="rect-container">
                             <div className="white-box">
-                                No Preview Available
+                                Soney & Lia
                             </div>
                             <div className="box-caption">
                                 Template 1
                             </div>
                         </div>
-                    </div>
+                    </a>
                     <div className="grid-col">
                         <div className="rect-container">
                             <div className="white-box">
@@ -111,12 +116,15 @@ export function CreateTrees() {
                         </div>
                     </div>
                 </div>
+                    <Separator/>
                 <div className="template-grid">
+
                     <div className="grid-col">
                         <div className="rect-container">
                             <div className="white-box">
                                 No Preview Available
                             </div>
+
                             <div className="box-caption">
                                 Template 5
                             </div>
