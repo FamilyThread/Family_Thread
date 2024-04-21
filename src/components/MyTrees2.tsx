e03c25bda9dede9465ed59f87a02488dfdc405ac
cuceta <cuceta@oswego.edu>
2024-04-21 15:43:33 -0400

    My trees now work for a low amount of trees
    If a user has 1-4 trees, the trees are displayed as a grid. If the user has more trees, they are displayed in a slider

--- src/components/MyTrees2.tsx
+++ src/components/MyTrees2.tsx
@@ -1,24 +1,27 @@
 import "../styles/sectionsInViewTrees.css"
 import axios from "axios";
 import {backend_url} from "../config/constant.ts";
-import {useEffect, useState, } from "react";
+import {useEffect, useState,} from "react";
 import image from "../assets/ViewTreesPlaceholder.png"
 import {useNavigate} from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
-interface Tree{
-    treeId : string;
-    treeName : string;
+interface Tree {
+    treeId: string;
+    treeName: string;
 }
 
 export function MyTreesViewTrees() {
-    const [treeArray, setTreeData]   = useState<Tree[]>([]);
+    const [treeArray, setTreeData] = useState<Tree[]>([]);
     const navigate = useNavigate();
-    const getiDs= async () =>{
-        const tree= await axios.get<{ treeId: string; treeName: string }[]>(backend_url + "/user/trees", { withCredentials: true });
+    const getiDs = async () => {
+        const tree = await axios.get<{
+            treeId: string;
+            treeName: string
+        }[]>(backend_url + "/user/trees", {withCredentials: true});
         const treeData = tree.data;
         setTreeData(treeData);
     }
@@ -40,18 +43,55 @@ export function MyTreesViewTrees() {
 
     };
 
+    if (treeArray.length >= 5) {
+        return (
+            <>
+                <Container>
+                    <h2 style={{marginTop: 90, marginBottom: 20}}>
+                        My Trees
+                    </h2>
+                    <div>
+                        <div>
+                            <Slider {...settings}>
+                                {treeArray.map((tree => (
+                                    <div>
+                                        <div style={{
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                        }}>
+                                            <img src={image} alt={"placeholder"} className={"tree-picture"}></img>
+                                        </div>
+                                        <div style={{
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                            paddingTop: 10
+                                        }}>
+                                            <button className="button-view-trees"
+                                                    onClick={() => handleTreeClick(tree.treeId)}>{tree.treeName}</button>
+                                        </div>
+                                    </div>
 
+                                )))}
+                            </Slider>
+                        </div>
+                    </div>
+                </Container>
+            </>
+        );
+    } else {
+        return (
 
-    return (
-        <>
             <Container>
                 <h2 style={{marginTop: 90, marginBottom: 20}}>
                     My Trees
                 </h2>
                 <div>
                     <div>
-                        <Slider {...settings}>
+                        <Row>
                             {treeArray.map((tree => (
+                            <Col>
                                 <div>
                                     <div style={{
                                         display: "flex",
@@ -66,15 +106,16 @@ export function MyTreesViewTrees() {
                                         alignItems: "center",
                                         paddingTop: 10
                                     }}>
-                                        <button className="button-view-trees" onClick={() => handleTreeClick(tree.treeId)}>{tree.treeName}</button>
+                                        <button className="button-view-trees"
+                                                onClick={() => handleTreeClick(tree.treeId)}>{tree.treeName}</button>
                                     </div>
                                 </div>
-
+                            </Col>
                             )))}
-                        </Slider>
+                        </Row>
                     </div>
                 </div>
             </Container>
-        </>
-    );
+        );
+    }
 }
