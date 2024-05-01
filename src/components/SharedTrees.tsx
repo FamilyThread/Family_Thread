0e66d022fe931ef417420ef922ef2df5af1e4d84
s-Aura-v <slamich2@oswego.edu>
2024-05-01 00:27:52 -0400

    Shared Trees + Style Fixes

--- src/components/SharedTrees.tsx
+++ src/components/SharedTrees.tsx
@@ -6,7 +6,7 @@ import {useNavigate} from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import {ImageConversion} from "./ImageConversion.tsx";
 
 
@@ -58,26 +58,62 @@ export function SharedTrees() {
         slidesToScroll: 1,
 
     };
-    if (treeArray.length > 1) {
+
+    if (treeArray.length >= 5) {
         return (
             <>
                 <Container>
-                    <h2 style={{marginTop: 20, marginBottom: 20}}>
+                    <h2 style={{marginTop: 90, marginBottom: 20}}>
                         Shared Trees
                     </h2>
                     <div>
-                        <div>
-                            <Slider {...settings}>
-                                {treeArray.map((tree => (
+                        <Slider {...settings}>
+                            {treeArray.map((tree => (
+                                <div>
+                                    <div style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                    }}>
+                                        <div className="tree-container">
+                                            {checkForImage(tree.image64, tree.treeId)}
+                                        </div>
+                                    </div>
+                                    <div style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        paddingTop: 10,
+                                    }}>
+                                        <button className="button-view-trees"
+                                                onClick={() => handleTreeClick(tree.treeId)}>{tree.treeName}</button>
+                                    </div>
+                                </div>
+                            )))}
+                        </Slider>
+                    </div>
+                </Container>
+            </>
+        );
+    }
+    if ((treeArray.length >= 1) && (treeArray.length <= 4)) {
+        return (
+            <Container>
+                <h2 style={{marginTop: 90, marginBottom: 20}}>
+                    Shared Trees
+                </h2>
+                <div>
+                    <div>
+                        <Row>
+                            {treeArray.map((tree => (
+                                <Col>
                                     <div>
                                         <div style={{
                                             display: "flex",
                                             justifyContent: "center",
                                             alignItems: "center",
                                         }}>
-                                            <div className="tree-container">
-                                                {checkForImage(tree.image64, tree.treeId)}
-                                            </div>
+                                            {checkForImage(tree.image64, tree.treeId)}
                                         </div>
                                         <div style={{
                                             display: "flex",
@@ -89,14 +125,15 @@ export function SharedTrees() {
                                                     onClick={() => handleTreeClick(tree.treeId)}>{tree.treeName}</button>
                                         </div>
                                     </div>
-                                )))}
-                            </Slider>
-                        </div>
+                                </Col>
+                            )))}
+                        </Row>
                     </div>
-                </Container>
-            </>
+                </div>
+            </Container>
         );
-    } else {
+    }
+    if (treeArray.length < 1) {
         return (
             <Container>
                 <h2 style={{marginTop: 90, marginBottom: 20}}>
